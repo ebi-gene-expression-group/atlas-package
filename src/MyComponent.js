14659a875474e3a45813ed83b49822bc3527b240
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-06-29 17:27:20 +0100

    Import PropTypes and add atlasUrl prop

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 class MyComponent extends React.Component {
 
+}
+
+MyComponent.propTypes = {
+  atlasUrl: PropTypes.string.isRequired
 }
 
 export default MyComponent
