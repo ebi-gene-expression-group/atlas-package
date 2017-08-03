d964029776036d92311fb940e04f2aaadd3e1a97
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-08-03 10:30:13 +0100

    Fix imports in tests

--- __test__/MyComponent.test.js
+++ __test__/MyComponent.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
-import {shallow, mount} from 'enzyme'
+import {shallow, mount, render} from 'enzyme'
 
 import MyComponent from '../src/MyComponent.js'
 
